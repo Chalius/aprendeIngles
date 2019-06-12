b14d51e9058e2a126c8e48d3610935ac325616ab
luismancor <juan.mancilla.c@tecsup.edu.pe>
2019-06-12 00:01:55 -0500

    navbar cambiada y logo agregado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,11 +27,12 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
+        <nav class="navbar navbar-expand-md navbar-light  shadow-sm"  style="background-color: #e3f2fd;">
+            <div class="container"  >
                 <!--AQUI SE TIENE QUE INGRESAR LA IMAGEN  <img src="http://placehold.it/150x50?text=Logo" alt="">EN VEZ DEL app.name-->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <img src="{{URL::asset('webimages/logo2.png')}}" height="40px" width="40px" style="" class="mx-auto d-block">
+                  
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
