5a252031a971dee964e0f9d145874c02b120ee7c
Chalius <gonzalo.quispe.f@tecsup.edu.pe>
2019-06-13 01:12:02 -0500

    mejorada vista perfil y añadiendo logros

--- resources/views/nosotros.blade.php
+++ resources/views/nosotros.blade.php
@@ -5,28 +5,22 @@
 
 
   <div class="container">
-    <div class="header clearfix">
+    <div class="row justify-content-center">
       <h2>Sobre nosotros</h2>
-    </div>
-      <div class="row marketing">
 
+    </div>
+    <hr style="">
+      <div class="row marketing">
         <div class="col-lg-6">
           <h4>Nuestros objetivos</h4>
           <p>El objetivo de esta página es que cualquier usuario pueda acceder a
           educación de calidad.</p>
-
-
-
-
         </div>
 
         <div class="col-lg-6">
           <h4>Nuestros valores</h4>
           <p>Somos un grupo de estudiantes que tiene como fundamentos el trabajo en equipo
           , la ética y el respeto a los demás.</p>
-
-
-
         </div>
       </div>
   </div>
--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -4,15 +4,102 @@
 
   <div class="container">
       <div class="row justify-content-center">
-          <div class="col-md-8">
-              <div class="card">
-                  <div class="card-body">
-                    <h1>Usuario</h1>
+          <div class="col-md-10">
+            <h1>
+              {{ Auth::user()->name}}
+              {{ Auth::user()->email }}
+            </h1>
+            <hr style="">
+
+            <div class="row marketing">
+              <div class="col-lg-6">
+                <h4>LOGROS</h4>
+
+                {{-- Items de logros --}}
+                <div class="row mb-2">
+                  <div class="col-md-10">
+                    <div class="card flex-md-row mb-4 box-shadow h-md-250">
+                      <div class="card-body d-flex flex-column align-items-start">
+                        <h3 class="mb-0">
+                          Primer ejercicio
+                        </h3>
+                        <p class="card-text mb-auto">Termina tu primer ejercicio.</p>
+                        <p class="card-text mb-auto">INCOMPLETO</p>
+                      </div>
+                      <img class="card-img-right flex-auto d-none d-lg-block" src="{{URL::asset('webimages/logros/logro1.png')}}"  height="128" width="128" alt="logro1">
+                    </div>
                   </div>
-                      LOGROS DESEMPEÑO
+                </div>
+
+                <div class="row mb-2">
+                  <div class="col-md-10">
+                    <div class="card flex-md-row mb-4 box-shadow h-md-250">
+                      <div class="card-body d-flex flex-column align-items-start">
+                        <h3 class="mb-0">
+                          Evaluate
+                        </h3>
+                        <p class="card-text mb-auto">Aprueba tu primer examen.</p>
+                        <p class="card-text mb-auto">INCOMPLETO</p>
+                      </div>
+                      <img class="card-img-right flex-auto d-none d-lg-block" src="{{URL::asset('webimages/logros/logro2.png')}}"  height="128" width="128" alt="logro1">
+                    </div>
+                  </div>
+                </div>
+
+                <div class="row mb-2">
+                  <div class="col-md-10">
+                    <div class="card flex-md-row mb-4 box-shadow h-md-250">
+                      <div class="card-body d-flex flex-column align-items-start">
+                        <h3 class="mb-0">
+                          Perfección
+                        </h3>
+                        <p class="card-text mb-auto">Aprueba un examen sin fallar.</p>
+                        <p class="card-text mb-auto">INCOMPLETO</p>
+                      </div>
+                      <img class="card-img-right flex-auto d-none d-lg-block" src="{{URL::asset('webimages/logros/logro3.png')}}"  height="128" width="128" alt="logro1">
+                    </div>
+                  </div>
+                </div>
+
+                <div class="row mb-2">
+                  <div class="col-md-10">
+                    <div class="card flex-md-row mb-4 box-shadow h-md-250">
+                      <div class="card-body d-flex flex-column align-items-start">
+                        <h3 class="mb-0">
+                          Persistente
+                        </h3>
+                        <p class="card-text mb-auto">Completa tus primeros 20 ejercicios.</p>
+                        <p class="card-text mb-auto">INCOMPLETO</p>
+                      </div>
+                      <img class="card-img-right flex-auto d-none d-lg-block" src="{{URL::asset('webimages/logros/logro4.png')}}"  height="128" width="128" alt="logro1">
+                    </div>
+                  </div>
+                </div>
+
+
+
+
+
+
 
 
               </div>
+              <div class="col-lg-6">
+                <h4>DESEMPEÑO</h4>
+                <p>Aquí veremos el desempeño obtenido.</p>
+              </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
           </div>
       </div>
   </div>
