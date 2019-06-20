13b9dbd06b37b7d026be1a15605680a9f6559669
unknown <lucero.ramos@tecsup.edu.pe>
2019-06-19 21:46:59 -0500

    cambio en welcome2.blade(correcciones)

--- resources/views/welcome2.blade.php
+++ resources/views/welcome2.blade.php
@@ -35,7 +35,7 @@
       <div class="row">
         <div class="px-lg-5 d-flex flex-column justify-content-center col-lg-6">
           <h1>Una de las principales razones a aprender Ingles</h1>
-          <p class="mb-3 lead">Esta pagina sigue la metodología de Ramon Campayo para el aprendizaje de idiomas, la cual esta basada principalmente en la memorizacion, el siguiente video muestra a mas detalle dicho metodo.</p>
+          <p class="mb-3 lead">Esta página sigue la metodología de Ramon Campayo para el aprendizaje de idiomas, la cual esta basada principalmente en la memorizacion, el siguiente video muestra a mas detalle dicho metodo.</p>
         </div>
         <div class="col-lg-6">
           <div class="embed-responsive embed-responsive-16by9">
@@ -49,21 +49,21 @@
     <div class="container">
       <div class="row">
         <div class="col-md-12 text-center">
-          <h1>Una nueva forma de aprender Ingles</h1>
+          <h1>Una nueva metodologia de aprender Ingles</h1>
         </div>
       </div>
       <div class="row justify-content-center">
         <div class="col-md-4 p-4"> <img class="img-fluid d-block rounded-circle" src="{{URL::asset('webimages/303.png')}}">
-          <h4><b>Entenderas la pronunciacion de los demas</b></h4>
-          <p>Incluso si tu vocabulario y gramatica son perfectos, puedes tener problemas para darte a entender por tu pronunciacion.<br>Aprender a pronunciar correctamente las palabras puede ser una de las partes mas dificiles para dominar el Ingles. NOSOTROS TE AYUDAREMOS.</p>
+          <h4><b>Entenderas la pronunciación de los demas</b></h4>
+          <p>Incluso si tu vocabulario y gramatica son perfectos, puedes tener problemas para darte a entender por tu pronunciacion.<br>Aprender a pronunciar correctamente las palabras puede ser una de las partes más dificiles para dominar el Ingles. NOSOTROS TE AYUDAREMOS.</p>
         </div>
         <div class="col-md-4 col-6 p-4"> <img class="img-fluid d-block rounded-circle" src="{{URL::asset('webimages/304.png')}}">
           <h4><b>Mejorara tu escritura y vocabulario</b></h4>
-          <p>Mejorar la escritura en ingles es uno de los objetivos que muchas personas tienen al aprender el idioma. No importa que nivel tengas o cual sea el uso que le darás al idioma. Esta habilidad siempre te dara margen para seguir mejorando.</p>
+          <p>Mejorar la escritura gramátical en ingles, pues es uno de los objetivos que muchas personas tienen al aprender el idioma. No importa que nivel tengas o cual sea el uso que le darás al idioma. Esta habilidad siempre te dara margen para seguir mejorando.</p>
         </div>
         <div class="col-md-4 col-6 p-4"> <img class="img-fluid d-block rounded-circle" src="{{URL::asset('webimages/305.png')}}">
-          <h4><b>Lograras interactuar con personas de diferentes partes</b></h4>
-          <p>Incluso los hablantes nativos de inglés a veces tienen problemas para comunicarse. Los problemas en la comunicacion se dan cuando se pierde la conexion entre el hablante y el oyente. En un momento dado, la informacion se pierde o distorciona.<br></p>
+          <h4><b>Lograras interactuar con personas de diferentes partes.</b></h4>
+          <p>Incluso los hablantes netos del idioma ingles, a veces tienen algunas dificultades para comunicarse. Los problemas en la comunicacion se dan cuando se pierde la conexión entre el hablante y el oyente. En un momento dado, la información se pierde o distorciona.<br></p>
         </div>
       </div>
 
